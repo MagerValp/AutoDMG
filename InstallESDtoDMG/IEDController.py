2c3bb82afd9ff27f87eebc4e5579511811b3b44d
Per Olofsson <MagerValp@gmail.com>
2013-10-22 16:33:09 +0200

    Moved table data sources into separate files.

--- InstallESDtoDMG/IEDController.py
+++ InstallESDtoDMG/IEDController.py
@@ -19,127 +19,13 @@ from IEDSocketListener import *
 from IEDDMGHelper import *
 from IEDProfileController import *
 from IEDUpdateCache import *
+from IEDUpdateTableDataSource import *
+from IEDPackageTableDataSource import *
 
 
 IEDTaskNone = 0
 IEDTaskInstall = 1
 IEDTaskImageScan = 2
-
-
-class IEDUpdateTableDataSource(NSObject):
-    
-    def initWithProfileController_updateCache_(self, pc, cache):
-        self = super(IEDUpdateTableDataSource, self).init()
-        if self is None:
-            return None
-        
-        self.profileController = pc
-        self.updateCache = cache
-        self.updates = list()
-        self.downloadSize = 0
-        self.downloadCount = 0
-        
-        self.cachedImage = NSImage.imageNamed_(u"Package")
-        self.uncachedImage = NSImage.imageNamed_(u"Package blue arrow")
-        
-        return self
-    
-    def loadProfileForVersion_build_(self, version, build):
-        profile = self.profileController.profileForVersion_Build_(version, build)
-        if profile:
-            self.updates = profile
-        else:
-            self.updates = list()
-        self.countDownloads()
-    
-    def countDownloads(self):
-        for update in self.updates:
-            if not self.updateCache.isCached_(update[u"sha1"]):
-                self.downloadCount += 1
-                self.downloadSize += update[u"size"]
-    
-    def numberOfRowsInTableView_(self, tableView):
-        return len(self.updates)
-    
-    def tableView_objectValueForTableColumn_row_(self, tableView, column, row):
-        if column.identifier() == u"image":
-            update = self.updates[row]
-            if self.updateCache.isCached_(update[u"sha1"]):
-                return self.cachedImage
-            else:
-                return self.uncachedImage
-        elif column.identifier() == u"name":
-            return self.updates[row][u"name"]
-
-
-class IEDPackageTableDataSource(NSObject):
-    
-    def init(self):
-        self = super(IEDPackageTableDataSource, self).init()
-        if self is None:
-            return None
-        
-        self.packages = list()
-        self.pkgImage = NSImage.imageNamed_(u"Package")
-        
-        return self
-    
-    def packagePaths(self):
-        return [pkg[u"path"] for pkg in self.packages]
-    
-    def numberOfRowsInTableView_(self, tableView):
-        return len(self.packages)
-    
-    def tableView_objectValueForTableColumn_row_(self, tableView, column, row):
-        return self.packages[row][column.identifier()]
-    
-    def tableView_setObjectValue_forTableColumn_row_(self, tableView, obj, column, row):
-        self.packages.insert(row, obj)
-    
-    def tableView_validateDrop_proposedRow_proposedDropOperation_(self, tableView, info, row, operation):
-        if info.draggingSource() == tableView:
-            return NSDragOperationMove
-        pboard = info.draggingPasteboard()
-        paths = pboard.propertyListForType_(NSFilenamesPboardType)
-        if not paths:
-            return NSDragOperationNone
-        for path in paths:
-            name, ext = os.path.splitext(path)
-            if ext.lower() not in (u".pkg", u".mpkg"):
-                return NSDragOperationNone
-        return NSDragOperationCopy
-    
-    def tableView_acceptDrop_row_dropOperation_(self, tableView, info, row, operation):
-        pboard = info.draggingPasteboard()
-        # If the source is the tableView, we're reordering packages within the
-        # table and the pboard contains the source row indices.
-        if info.draggingSource() == tableView:
-            indices = [int(i) for i in pboard.propertyListForType_(NSStringPboardType).split(u",")]
-            for i in indices:
-                self.packages[row], self.packages[i] = self.packages[i], self.packages[row]
-        else:
-            # Otherwise it's a list of paths to add to the table.
-            paths = pboard.propertyListForType_(NSFilenamesPboardType)
-            for path in paths:
-                package = {
-                    u"image": NSWorkspace.sharedWorkspace().iconForFile_(path),
-                    u"path": path,
-                    u"name": os.path.basename(path),
-                }
-                self.packages.insert(row, package)
-        tableView.reloadData()
-        return True
-    
-    def tableView_writeRowsWithIndexes_toPasteboard_(self, tableView, rowIndexes, pboard):
-        # When reordering packages put a list of indices as a string onto the pboard.
-        indices = list()
-        index = rowIndexes.firstIndex()
-        while index != NSNotFound:
-            indices.append(index)
-            index = rowIndexes.indexGreaterThanIndex_(index)
-        pboard.declareTypes_owner_([NSStringPboardType], self)
-        pboard.setPropertyList_forType_(u",".join(unicode(i) for i in indices), NSStringPboardType)
-        return True
 
 
 class IEDController(NSObject):
