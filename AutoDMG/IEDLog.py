97edbc040ce515a53d03a393965393d12c2d2e0d
Per Olofsson <MagerValp@gmail.com>
2013-10-28 14:03:37 +0100

    Added log window.

--- AutoDMG/IEDLog.py
+++ AutoDMG/IEDLog.py
@@ -9,6 +9,10 @@
 
 
 from Foundation import *
+from AppKit import *
+from objc import IBAction, IBOutlet
+
+from IEDLogLine import *
 import inspect
 
 
@@ -25,18 +29,160 @@ IEDLogLevelDebug     = 7
 defaults = NSUserDefaults.standardUserDefaults()
 
 
+class IEDLog(NSObject):
+    
+    # Singleton instance.
+    _instance = None
+    
+    logWindow = IBOutlet()
+    logTableView = IBOutlet()
+    levelSelector = IBOutlet()
+    
+    logLines = list()
+    visibleLogLines = list()
+    
+    def init(self):
+        # Initialize singleton.
+        if IEDLog._instance is not None:
+            return IEDLog._instance
+        self = super(IEDLog, self).init()
+        if self is None:
+            return None
+        IEDLog._instance = self
+        return self
+    
+    def awakeFromNib(self):
+        global defaults
+        self.levelSelector.selectItemAtIndex_(defaults.integerForKey_(u"LogLevel"))
+        self.logWindowVisible = False
+        self.logTableView.setDataSource_(self)
+    
+    # Helper methods.
+    
+    def levelName_(self, level):
+        return (
+            u"Emergency",
+            u"Alert",
+            u"Critical",
+            u"Error",
+            u"Warning",
+            u"Notice",
+            u"Info",
+            u"Debug",
+        )[level]
+    
+    def addMessage_level_(self, message, level):
+        logLine = IEDLogLine.alloc().initWithMessage_level_(message, level)
+        self.logLines.append(logLine)
+        if defaults.integerForKey_(u"LogLevel") >= level:
+            self.visibleLogLines.append(logLine)
+            self.logTableView.reloadData()
+    
+    
+    
+    # Act on user toggling log window.
+    
+    @IBAction
+    def toggleLogWindow_(self, sender):
+        if self.logWindowVisible:
+            self.logWindow.orderOut_(self)
+            self.logWindowVisible = False
+        else:
+            self.logWindow.makeKeyAndOrderFront_(self)
+            self.logWindowVisible = True
+    
+    # NSWindowDelegate methods.
+    
+    def windowWillClose_(self, sender):
+        self.logWindowVisible = False
+    
+    
+    
+    # Act on user filtering log.
+    
+    @IBAction
+    def setLevel_(self, sender):
+        self.visibleLogLines = [x for x in self.logLines if x.level() <= self.levelSelector.indexOfSelectedItem()]
+        self.logTableView.reloadData()
+    
+    
+    
+    # Act on user clicking save button.
+    
+    @IBAction
+    def saveLog_(self, sender):
+        panel = NSSavePanel.savePanel()
+        panel.setExtensionHidden_(False)
+        panel.setAllowedFileTypes_([u"log", u"txt"])
+        formatter = NSDateFormatter.alloc().init()
+        formatter.setDateFormat_(u"yyyy-MM-dd HH.mm.ss")
+        dateStr = formatter.stringFromDate_(NSDate.date())
+        panel.setNameFieldStringValue_(u"AutoDMG %s" % dateStr)
+        result = panel.runModal()
+        if result != NSFileHandlingPanelOKButton:
+            return
+        
+        exists, error = panel.URL().checkResourceIsReachableAndReturnError_(None)
+        if exists:
+            success, error = NSFileManager.defaultManager().removeItemAtURL_error_(panel.URL(), None)
+            if not success:
+                NSApp.presentError_(error)
+                return
+        
+        success, error = NSData.data().writeToURL_options_error_(panel.URL(), 0, None)
+        if not success:
+            NSApp.presentError_(error)
+            return
+        
+        fh, error = NSFileHandle.fileHandleForWritingToURL_error_(panel.URL(), None)
+        if fh is None:
+            NSAlert.alertWithError_(error).runModal()
+            return
+        formatter = NSDateFormatter.alloc().init()
+        formatter.setDateFormat_(u"yy-MM-dd HH:mm")
+        for logLine in self.logLines:
+            textLine = NSString.stringWithFormat_(u"%@ %@: %@\n",
+                                                  formatter.stringFromDate_(logLine.date()),
+                                                  self.levelName_(logLine.level()),
+                                                  logLine.message())
+            fh.writeData_(textLine.dataUsingEncoding_(NSUTF8StringEncoding))
+        fh.closeFile()
+    
+    
+    
+    # We're an NSTableViewDataSource.
+    
+    def numberOfRowsInTableView_(self, tableView):
+        return len(self.visibleLogLines)
+    
+    def tableView_objectValueForTableColumn_row_(self, tableView, column, row):
+        if column.identifier() == u"date":
+            return self.visibleLogLines[row].date()
+        elif column.identifier() == u"level":
+            return self.levelName_(self.visibleLogLines[row].level())
+        elif column.identifier() == u"message":
+            return self.visibleLogLines[row].message()
+
+
+_log = IEDLog.alloc().init()
+
 def LogMessage(level, message):
-    if defaults.integerForKey_(u"LogLevel") >= level:
-        prefix = u""
-        if level == IEDLogLevelDebug:
-            for caller in inspect.stack()[1:]:
-                modname = inspect.getmodule(caller[0]).__name__
-                if modname == u"IEDLog":
-                    continue
-                lineno = caller[2]
-                prefix = u"(%s:%d) " % (modname, lineno)
-                break
-        NSLog(u"%@%@", prefix, message)
+    global _log
+    
+    prefix = u""
+    if level == IEDLogLevelDebug:
+        for caller in inspect.stack()[1:]:
+            modname = inspect.getmodule(caller[0]).__name__
+            if modname == u"IEDLog":
+                continue
+            lineno = caller[2]
+            prefix = u"(%s:%d) " % (modname, lineno)
+            break
+    
+    for line in message.split(u"\n"):
+        _log.addMessage_level_(prefix + line, level)
+        if defaults.integerForKey_(u"LogLevel") >= level:
+            NSLog(u"%@", prefix + line)
 
 def LogDebug(*args):
     LogMessage(IEDLogLevelDebug, NSString.stringWithFormat_(*args))
---
+++ AutoDMG/IEDLogLine.py
@@ -0,0 +1,49 @@
+#-*- coding: utf-8 -*-
+#
+#  IEDLogLine.py
+#  AutoDMG
+#
+#  Created by Pelle on 2013-10-28.
+#  Copyright (c) 2013 University of Gothenburg. All rights reserved.
+#
+
+from Foundation import *
+
+
+class IEDLogLine(NSObject):
+    
+    def init(self):
+        self = super(IEDLogLine, self).init()
+        if self is None:
+            return None
+        
+        self._date = NSDate.date()
+        self._message = u""
+        self._level = 0
+        
+        return self
+    
+    def initWithMessage_level_(self, message, level):
+        self = self.init()
+        if self is None:
+            return None
+        
+        self._message = message
+        self._level = level
+        
+        return self
+    
+    def date(self):
+        return self._date
+    def setDate(self, date):
+        self._date = date
+    
+    def message(self):
+        return self._message
+    def setMessage(self, message):
+        self._message = message
+    
+    def level(self):
+        return self._level
+    def setLevel(self, level):
+        self._level = level
