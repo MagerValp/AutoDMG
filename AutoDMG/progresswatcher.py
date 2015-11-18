9b47d23e2d1bf99c12b5dfc602ea0090ae338390
Per Olofsson <magervalp@fastmail.fm>
2015-11-18 16:18:50 +0100

    Send startup asr messages to log.

--- AutoDMG/progresswatcher.py
+++ AutoDMG/progresswatcher.py
@@ -80,7 +80,9 @@ class ProgressWatcher(NSObject):
         if data.length():
             progressStr = NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding)
             if (not self.asrProgressActive) and (u"\x0a" in progressStr):
-                progressStr = u"".join(progressStr.partition(u"\x0a")[1:])
+                msg, _, rest = progressStr.partition(u"\x0a")
+                progressStr = u"\x0a" + rest
+                self.postNotification_({u"action": u"log_message", u"log_level": 6, u"message": u"asr output: " + msg.rstrip()})
             while progressStr:
                 if progressStr.startswith(u"\x0a"):
                     progressStr = progressStr[1:]
